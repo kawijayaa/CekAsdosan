3f0f7bee3a282749c417432faed7baf0c87803f7
Muhammad Oka <oka@muhammadoka.dev>
2023-12-31 09:28:33 +0000

    dont broadcast when message is the same

--- main.py
+++ main.py
@@ -11,6 +11,7 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.support import expected_conditions
+from pyvirtualdisplay import Display
 
 load_dotenv()
 
@@ -35,6 +36,9 @@ logging.basicConfig(level=logging.INFO,
                         logging.FileHandler("log"),
                         logging.StreamHandler(sys.stdout)])
 
+display = Display(visible=0, size=(800,600))
+display.start()
+
 def get_course_list(semester, kurikulum, tahun=datetime.datetime.now().year):
     log.info("CHECK STARTED")
 
@@ -54,7 +58,7 @@ def get_course_list(semester, kurikulum, tahun=datetime.datetime.now().year):
 
     WebDriverWait(driver, 20).until(expected_conditions.presence_of_element_located((By.XPATH, "/html/body/div[@class='container']/div[@id='content']/div[@id='content']/div[@id='main-content']/h2[@class='content-title']")))
 
-    classes = driver.find_elements(By.XPATH, "//td[contains(text(), '" + kurikulum + "') and ancestor::table/preceding-sibling::h4[1][contains(text(), '" + f"{semester} {tahun}/{tahun+1}" + "')]]")
+    classes = driver.find_elements(By.XPATH, "//td[contains(text(), '" + kurikulum + "') and ancestor::table/preceding-sibling::h4[1][contains(text(), '" + f"{semester} {tahun}/{}" + "')]]".format(tahun+1))
 
     if classes != []:
         log.info(f"{semester.upper()} {tahun}/{tahun+1} IS OPEN")
@@ -73,16 +77,16 @@ def get_course_list(semester, kurikulum, tahun=datetime.datetime.now().year):
 @bot.event
 async def on_ready():
     os.system("clear")
+    bot.last_message = ""
     send_message.start()
     log.info("BOT READY")
-
 
 @bot.event
 async def on_message(msg: discord.Message):
     if not msg.author.bot and msg.content.lower() == "check" and msg.channel.id == CHANNEL_ID:
         log.info(f"CHECK REQUEST BY {msg.author.name}")
         await send_message()
-    
+
     if msg.guild is None and not msg.author.bot and msg.content.lower().startswith("check"):
         log.info(f"DM CHECK REQUEST BY {msg.author.name}")
 
@@ -117,12 +121,15 @@ async def on_message(msg: discord.Message):
                 log.info(f"SENT TO {msg.author.name}")
 
 
-@tasks.loop(hours=3)
+@tasks.loop(hours=1)
 async def send_message():
     channel = bot.get_channel(CHANNEL_ID)
-    embed = get_course_list("Ganjil", "02.00.12.01-2020")
-    await channel.send(embed=embed)
-    log.info(f"SENT TO {channel.guild}/{channel.name}")
-    
+    embed = get_course_list("Genap", "02.00.12.01-2020", bot.last_message)
+    if embed is not None:
+        bot.last_message = embed.description
+        await channel.send(embed=embed)
+        log.info(f"SENT TO {channel.guild}/{channel.name}")
+    else:
+        log.info("Message not sent because message is the same as before")
 
 bot.run(TOKEN, log_handler=None)
