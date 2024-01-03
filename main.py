96ca156905095449804ab9b847e1378568dd80d0
Muhammad Oka <oka@muhammadoka.dev>
2024-01-03 12:53:31 +0700

    use requests session

--- main.py
+++ main.py
@@ -1,27 +1,26 @@
 import datetime
-import requests
 import os
-import re
 import discord
 import logging
 import sys
+import json
 from discord.ext import tasks
-from bs4 import BeautifulSoup, NavigableString
 from dotenv import load_dotenv
+from scraper import Scraper
 
 load_dotenv()
 
-WEBSITE_URL = "https://siasisten.cs.ui.ac.id/"
-USERNAME = os.getenv("SIAK_USERNAME")
-PASSWORD = os.getenv("SIAK_PASSWORD")
-TOKEN = os.getenv("DISCORD_BOT_TOKEN")
-CHANNEL_ID = 1014578047028052048
+WEBSITE_URL = os.getenv("CEKASDOSAN_WEBSITE_URL")
+USERNAME = os.getenv("CEKASDOSAN_SIAK_USERNAME")
+PASSWORD = os.getenv("CEKASDOSAN_SIAK_PASSWORD")
+TOKEN = os.getenv("CEKASDOSAN_BOT_TOKEN")
+CHANNEL_ID = os.getenv("CEKASDOSAN_CHANNEL_ID")
+KODE_KURIKULUM = os.getenv("CEKASDOSAN_KODE_KURIKULUM")
+
 TIMEZONE = datetime.timezone(datetime.timedelta(hours=7))
 
-SEMESTER = "Genap"
-KODE_KURIKULUM = "02.00.12.01-2020"
-
 bot = discord.Client(intents=discord.Intents.all())
+
 log = logging.getLogger()
 logging.basicConfig(level=logging.INFO,
                     format="%(asctime)s: %(message)s",
@@ -30,74 +29,52 @@ logging.basicConfig(level=logging.INFO,
                         logging.FileHandler("log"),
                         logging.StreamHandler(sys.stdout)])
 
-@tasks.loop(hours=1)
+scr = Scraper()
+scr.login(WEBSITE_URL+"login/", USERNAME, PASSWORD)
+
+
+@tasks.loop(minutes=30)
 async def send_message():
+    log.info("CHECK STARTED")
     channel = bot.get_channel(CHANNEL_ID)
 
-    with requests.session() as session:
-        login_page = session.get(WEBSITE_URL+"login/")
-        soup = BeautifulSoup(login_page.text, features="html.parser")
+    lowongans = scr.scrape(WEBSITE_URL+"lowongan/listLowongan/", KODE_KURIKULUM)
 
-        csrf_token = soup.find('input', {'name': 'csrfmiddlewaretoken'})['value']
+    embed = discord.Embed(
+        title=f"CekAsdosan v2 - {lowongans.get('tahun_ajaran')}")
 
-        payload = {
-            "csrfmiddlewaretoken": csrf_token,
-            "username": USERNAME,
-            "password": PASSWORD,
-            "next": ""
-        }
+    if lowongans != bot.last_message:
+        for lowongan in lowongans['lowongan']:
+            kode_matkul = lowongan.get('kode_matkul')
+            nama_matkul = lowongan.get('nama_matkul')
+            dosen = lowongan.get('dosen')
+            status = lowongan.get('status')
+            jumlah_lowongan = lowongan.get('jumlah_lowongan')
+            jumlah_pelamar = lowongan.get('jumlah_pelamar')
+            jumlah_diterima = lowongan.get('jumlah_diterima')
 
-        headers = {
-            "Referer": WEBSITE_URL+"login/"
-        }
-
-        login_request = session.post(
-            WEBSITE_URL+"login/", payload, headers=headers)
-        soup = BeautifulSoup(login_request.text,
-                             features="html.parser")
-
-        tahun_ajaran: list[NavigableString] = list(dict.fromkeys(soup.findAll(
-            string=re.compile(f"{SEMESTER} 2023/2024"))))
-        table = tahun_ajaran[0].next_element.next_element
-
-        embed = discord.Embed(title=f"CekAsdosan - {SEMESTER} 2023/2024")
-        msgs = []
-
-        lowongans = table.findAll(string=re.compile(KODE_KURIKULUM))
-        for lowongan in lowongans:
-            kode_matkul = str(lowongan).split("-")[0].strip()
-            nama_matkul = str(lowongan.next_element.next_element).strip()
-            dosen = str(
-                lowongan.next_element.next_element.next_element.next_element.next_element).strip()
-            lowongan_buka = str(
-                lowongan.next_element.next_element.next_element.next_element.next_element.next_element.next_element.next_element).strip()
-            lowongan_buka = "Open" if lowongan_buka == "Buka" else "Closed"
-            jumlah_lowongan = str(
-                lowongan.next_element.next_element.next_element.next_element.next_element.next_element.next_element.next_element.next_element.next_element.next_element).strip().split()[0]
-            jumlah_pelamar = str(
-                lowongan.next_element.next_element.next_element.next_element.next_element.next_element.next_element.next_element.next_element.next_element.next_element.next_element.next_element.next_element).strip().split()[0]
-            jumlah_diterima = str(
-                lowongan.next_element.next_element.next_element.next_element.next_element.next_element.next_element.next_element.next_element.next_element.next_element.next_element.next_element.next_element.next_element.next_element.next_element).strip().split()[0]
-
-            msgs.append(f"{kode_matkul} {nama_matkul} {dosen} {lowongan_buka} {jumlah_lowongan} {jumlah_pelamar} {jumlah_diterima}")
-            embed.add_field(name=kode_matkul, value=f"{nama_matkul} - {dosen}\nStatus: {lowongan_buka}\nOpen: {jumlah_lowongan}\nRegistrant: {jumlah_pelamar}\nAccepted: {jumlah_diterima}", inline=False)
             log.info(f"{kode_matkul} - {nama_matkul}")
+            embed.add_field(name=kode_matkul,
+                            value=f"{nama_matkul} - {dosen}\n\
+                                    Status: {status}\n\
+                                     Open: {jumlah_lowongan}\n\
+                                    Registrants: {jumlah_pelamar}\n\
+                                    Accepted: {jumlah_diterima}", 
+                            inline=False)
+    
+        await channel.send(embed=embed)
+        log.info("MESSAGE SENT")
 
-        if "\n".join(msgs) != bot.last_message:
-            with open("last_message.cekasdosan", "w") as f:
-                bot.last_message = "\n".join(msgs)
-                f.write(bot.last_message)
-            await channel.send(embed=embed)
-
-            log.info("MESSAGE SENT")
-        else:
-            log.info("MESSAEGE NOT SENT: MESSAGE SAME AS LAST MESSAGE")
+        with open("last_message.json", "w") as f:
+            json.dump(lowongans, f)
+    else:
+        log.info("MESSAGE NOT SENT: MESSAGE SAME AS LAST MESSAGE")
 
 
 @bot.event
 async def on_ready():
-    with open("last_message.cekasdosan", "r") as f:
-        bot.last_message = f.read()
+    with open("last_message.json", "r") as f:
+        bot.last_message = json.load(f)
     send_message.start()
     log.info("BOT READY")
 
---
+++ scraper.py
@@ -0,0 +1,57 @@
+import requests
+import re
+from bs4 import BeautifulSoup
+
+class Scraper(object):
+    def __init__(self):
+        self.session = requests.session()
+
+    def login(self, login_url, username, password):
+        login_page = self.session.get(login_url)
+        soup = BeautifulSoup(login_page.text, features="html.parser")
+
+        csrf_token = soup.find(
+            'input', {'name': 'csrfmiddlewaretoken'})['value']
+
+        payload = {
+            "csrfmiddlewaretoken": csrf_token,
+            "username": username,
+            "password": password,
+            "next": ""
+        }
+
+        headers = {
+            "Referer": login_url
+        }
+
+        self.session.post(
+            login_url, payload, headers=headers)
+
+    def scrape(self, url, kode_kurikulum):
+        soup = BeautifulSoup(self.session.get(url).content,
+                             features="html.parser")
+
+        table = soup.findAll("table")[0]
+        tahun_ajaran = table.previous_element.previous_element
+
+        msgs = []
+
+        lowongans = table.findAll(string=re.compile(kode_kurikulum))
+        for lowongan in lowongans:
+            kode_matkul = str(lowongan).split("-")[0].strip()
+            nama_matkul = str(lowongan.next_element.next_element).strip()
+            dosen = str(
+                lowongan.next_element.next_element.next_element.next_element.next_element).strip()
+            status = str(
+                lowongan.next_element.next_element.next_element.next_element.next_element.next_element.next_element.next_element).strip()
+            jumlah_lowongan = str(
+                lowongan.next_element.next_element.next_element.next_element.next_element.next_element.next_element.next_element.next_element.next_element.next_element).strip().split()[0]
+            jumlah_pelamar = str(
+                lowongan.next_element.next_element.next_element.next_element.next_element.next_element.next_element.next_element.next_element.next_element.next_element.next_element.next_element.next_element).strip().split()[0]
+            jumlah_diterima = str(
+                lowongan.next_element.next_element.next_element.next_element.next_element.next_element.next_element.next_element.next_element.next_element.next_element.next_element.next_element.next_element.next_element.next_element.next_element).strip().split()[0]
+
+            msgs.append(dict(kode_matkul=kode_matkul, nama_matkul=nama_matkul, dosen=dosen, status=status,
+                        jumlah_lowongan=jumlah_lowongan, jumlah_pelamar=jumlah_pelamar, jumlah_diterima=jumlah_diterima))
+        
+        return dict(tahun_ajaran=tahun_ajaran, lowongan=msgs)
